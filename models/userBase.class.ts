0e3262cab14a89f67b2ad64e9aa7b6349dfe55c7
Mikki Monroe <msmikkimonroe@gmail.com>
2020-12-24 13:45:06 -0500

    added gravatar and role

--- models/userBase.class.ts
+++ models/userBase.class.ts
@@ -1,15 +1,25 @@
 import * as _ from 'lodash';
+import { Md5 } from 'ts-md5/dist/md5';
+var md5 = require('md5');
 import { Id, IId } from './id.class';
 
 export interface IUserBase extends IId {
     name: string;
+    avatar: string;
+    role: string;
 }
 
 export class UserBase extends Id implements IUserBase {
     name: string    = '';
+    avatar: string  = '';
+    role: string    = 'user';
 
     constructor(userBase?: any) {
         super(userBase);
         this.initialize(this, userBase);
+        if( _.isEmpty(this.avatar) ) {
+            md5 = new Md5();
+            this.avatar = `https://www.gravatar.com/avatar/${md5.appendStr(`${this.id.toLocaleLowerCase()}@anonymousmeetings.app`).end()}?d=identicon`;;
+        }
     }
 }
