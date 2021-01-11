b4eb0edb54051034bcefbcc78d47328fab583463
Mikki <msmikkimonroe@gmail.com>
2021-01-11 12:56:13 -0500

    recurrence

---
+++ listings/recurrence.ts
@@ -0,0 +1,10 @@
+export interface IRecurrence {
+    type:               number;     // allowed 1, 2, 3 required
+    repeat_interval:    number;
+    weekly_days:        string;
+    monthly_day:        number;
+    monthly_week:       number;
+    monthly_week_day:   number;
+    end_times:          number;
+    end_date_time:      string;
+} 
