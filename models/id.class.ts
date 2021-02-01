1a4557449cc18bb277210583c707dbd823d11915
Mikki <msmikkimonroe@gmail.com>
2021-01-31 20:25:13 -0600

    removed timestamps

--- models/id.class.ts
+++ models/id.class.ts
@@ -7,40 +7,37 @@ export interface IId extends IBase {
     id: string;
 
     // todo move to Audit class
-    ts: {
-        createdAt: any;
-        updatedAt: any;
-    }
+    //createdAt: firebase.firestore.Timestamp | any;
+    //updatedAt: firebase.firestore.Timestamp | any;
 }
 
 export class Id extends Base implements IId {
     id: string  = uuidv4();
-
-    ts = { 
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-    }
+    //createdAt = firebase.firestore.FieldValue.serverTimestamp();
+    //updatedAt = firebase.firestore.FieldValue.serverTimestamp();
 
     constructor(id?: any) { // IId
         super();
         this.initialize(this, id);
 
-        // if (!this.ts.createdAt) {
-            this.ts.createdAt = firebase.firestore.FieldValue.serverTimestamp();
+        // if (!this.createdAt) {
+            // this.createdAt = firebase.firestore.FieldValue.serverTimestamp();
         // }
     }
 
     public toObject(exclude?: string[]): any {
-        const ts_createdAt = this.ts.createdAt;
+        // const ts_createdAt = this.createdAt;
         const obj = super.toObject(exclude);
-        obj.ts.createdAt = ts_createdAt;
-        obj.ts.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
+        // obj.createdAt = ts_createdAt;
+        // obj.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
         return obj;
     }
 
-    public toGeoObject(geo?: geofirex.GeoFireClient, exclude?: string[]) {
-        const obj = super.toGeoObject(geo, exclude);
-        obj.ts.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
-        return obj;
-    }
+    // public toGeoObject(geo?: geofirex.GeoFireClient, exclude?: string[]) {
+    //     // const ts_createdAt = this.createdAt;
+    //     const obj = super.toGeoObject(geo, exclude);
+    //     // obj.createdAt = ts_createdAt;
+    //     // obj.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
+    //     return obj;
+    // }
 }
