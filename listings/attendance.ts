e1b4dfc59d547126afef189ea483c9a3abc9f80d
Mikki <mikkimichaelis@gmail.com>
2021-09-04 15:09:41 -0500

    UPDATE ATTENDANCE
    working on structure
    +isValid()

--- listings/attendance.ts
+++ listings/attendance.ts
@@ -1,55 +1,76 @@
-import { DateTime } from 'luxon';
+import { head, last } from 'lodash';
+import { DateTime, Duration } from 'luxon';
 import { Id, IId } from '../models/id.class';
 
 
 export interface IAttendance extends IId {
     uid: string;    // User ID
     mid: string;    // Meeting.id
-    zid: string;    // Zoom ID
+    zid: string;    // Zoom id
+    zID: string;    // Zoom ID
     pid: string;    // Zoom meeting participation(?) id
 
     records: IAttendanceRecord[];
 
-    signature: string;
+    status: string;         // 'VALID' || 'INVALID'
+    log: string[];          // invalid reasons otherwise []
+    credit: number;         // millis of attendance
+
+    timestamp: string;
+
+    isValid(): boolean;
 }
 
 export class Attendance extends Id implements IAttendance {
     uid: string = '';
     mid: string = '';
     zid: string = '';
+    zID: string = '';
     pid: string = '';
 
     records: IAttendanceRecord[] = [];
 
-    ip: string = '';
-    iip: number[] = [];
+    status: string = 'INVALID';
+    log: string[] = [];
+    credit: number = 0;
 
-    signature: string = ''
+    timestamp: string = '';
 
-    constructor(attendance: any) {
+    constructor(attendance?: any) {
         super(attendance);
         this.initialize(this, attendance);
+    }
+
+    // Here we check for 
+    // attendance time > 5m
+    public isValid(): boolean {
+        let valid = true;
+
+        valid = valid && this.records.length > 1;   // require two records to be valid
+
+        const start = DateTime.fromISO(head(this.records).timestamp);
+        const end = DateTime.fromISO(last(this.records).timestamp);
+        const duration: Duration = end.diff(start);
+
+        valid = valid && duration.minutes > 5;
+
+        return valid;
+    }
+
+    private validateRecords(): boolean {
+        let valid = true;
+
+        return valid;
     }
 }
 
 export interface IAttendanceRecord extends IId {
     status: string;
-    verified: boolean;
-    verified_method: string;
     visible: boolean;
     volume: number;
     audio: boolean;
-    muted: boolean;
     loud: boolean;
     timestamp: string;
-    created: string;
-
-    ipv4: string;
-    iipv4: number[];
-    ipv6: string;
-    iipv6: string[];
-
-    signature: string;
 }
 
 export class AttendanceRecord extends Id {
@@ -59,17 +80,14 @@ export class AttendanceRecord extends Id {
     did: string = '';
 
     status: string = '';
-    verified: boolean = false; 
     visible: boolean = false;
     volume: number = 0;
     audio: boolean = false;
-    muted: boolean = true;
     loud: boolean = false;
     timestamp: string = '';
-    created: string = DateTime.utc().toISO();
    
-    constructor(attendance?: any) {
-        super(attendance);
-        this.initialize(this, attendance);
+    constructor(attendanceRecord?: any) {
+        super(attendanceRecord);
+        this.initialize(this, attendanceRecord);
     }
 }
