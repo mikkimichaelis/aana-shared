3851b01503119555e11bdd268d20107b00e38406
Mikki <mikkimichaelis@gmail.com>
2021-09-05 18:02:32 -0500

    ADD process TO Attendance

--- listings/attendance.ts
+++ listings/attendance.ts
@@ -13,12 +13,16 @@ export interface IAttendance extends IId {
     records: IAttendanceRecord[];
 
     status: string;         // 'VALID' || 'INVALID'
-    log: string[];          // invalid reasons otherwise []
+    log: string[];          // verbose record of attendanceRecords
     credit: number;         // millis of attendance
 
-    timestamp: string;      // UTC ISO string
+    local: string;          // Local pretty date
+    timestamp: number;      // UTC Millis
 
-    isValid(): boolean;
+    isGarbage(): boolean;
+    process(): boolean;
+
+    addRecord(record: any);
 }
 
 export class Attendance extends Id implements IAttendance {
@@ -34,53 +38,108 @@ export class Attendance extends Id implements IAttendance {
     log: string[] = [];
     credit: number = 0;
 
-    timestamp: string = '';
+    local: string = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+    timestamp: number = DateTime.now().toMillis();
 
     constructor(attendance?: any) {
         super(attendance);
         this.initialize(this, attendance);
     }
 
-    // Here we check for 
-    // attendance time > 5m
-    public isValid(): boolean {
+    public addRecord(record: any) {
+        this.records.push(new AttendanceRecord(record));
+    }
+
+    // Here we check for garbage
+    // attendance time > 1m
+    public isGarbage(): boolean {
         let valid = true;
 
         valid = valid && this.records.length > 1;   // require two records to be valid
 
+        // @ts-ignore
         const start = DateTime.fromISO(head(this.records).timestamp);
+        // @ts-ignore
         const end = DateTime.fromISO(last(this.records).timestamp);
         const duration: Duration = end.diff(start);
 
-        valid = valid && duration.minutes > 5;
+        valid = valid && duration.minutes > 1;
 
         return valid;
     }
 
-    private validateRecords(): boolean {
-        let valid = true;
+    public process(): boolean {
+        this.log = [];      // be sure to clear running lists.....
+        this.credit = 0;    // and counters!
 
-        return valid;
+        // here we create a log entry for each period (intended for support viewing)
+        // a period is the time between validity changes
+        let period_start: number = null;
+        this.records.forEach((r, index) => {
+            let log = ``;
+            if (r.status === 'MEETING_ACTIVE_TRUE') {
+                period_start = r.timestamp;
+                this.log.push(`*PERIOD START ${r.local}`)
+            } else if (r.status === 'MEETING_ACTIVE_FALSE') {
+                if (period_start) {
+                    const duration = Duration.fromMillis(r.timestamp - period_start);
+                    this.credit = this.credit + duration.toMillis();
+                    this.log.push(`*PERIOD END ${duration.toFormat('hh:mm:ss')} ${r.local}`);
+                }
+                period_start = null;
+            } else {
+                if (!r.visible) {
+                    log = log + '!VISIBLE ';
+                }
+
+                if (!r.audio) {
+                    log = log + '!AUDIO ';
+                }
+
+                if (r.volume < .4) {
+                    log = log + '!VOLUME ';
+                }
+
+                if (log === '') {
+                    if (!period_start) {
+                        period_start = r.timestamp;
+                        this.log.push(`*PERIOD START ${r.local}`)
+                    }
+                } else {
+                    if (period_start) {
+                        // end existing period
+                        const duration = Duration.fromMillis(r.timestamp - period_start);
+                        this.credit = this.credit + duration.toMillis();
+                        this.log.push(`*PERIOD END ${duration.toFormat('hh:mm:ss')} ${r.local} ${log}`);
+                        period_start = null;
+                    } 
+                }
+            }
+        });
+        return true;
     }
 }
 
 export interface IAttendanceRecord extends IId {
-    status: string;
-    visible: boolean;
-    volume: number;
-    audio: boolean;
-    loud: boolean;
-    timestamp: string;
+    status: string;     // [Zoom.MeetingStatus] || ['MEETING_ACTIVE_TRUE', 'MEETING_ACTIVE_FALSE']
+    visible: boolean;   // Video Activity visible
+    volume: number;     // device call volume (not media!)
+    audio: boolean;     // Zoom audio connected
+    loud: boolean;      // speakerphone
+    local: string;      // local datetime string
+    timestamp: number;  // UTC Millis
 }
 
 export class AttendanceRecord extends Id {
     status: string = '';
-    visible: boolean = false;
+    visible: boolean = true;
     volume: number = 0;
-    audio: boolean = false;
-    loud: boolean = false;
-    timestamp: string = '';
-   
+    audio: boolean = true;
+    loud: boolean = true;
+
+    local: string = DateTime.local().toLocaleString(DateTime.TIME_WITH_SHORT_OFFSET);
+    timestamp: number = DateTime.now().toMillis();
+
     constructor(attendanceRecord?: any) {
         super(attendanceRecord);
         this.initialize(this, attendanceRecord);
