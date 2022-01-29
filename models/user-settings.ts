8c9fd4234a4ebf0b7fa559311ef7778f1204021e
Mikki Michaelis <mikkimichaelis@gmail.com>
2022-01-29 01:34:53 -0600

    removed dead code

--- models/user-settings.ts
+++ models/user-settings.ts
@@ -1,21 +1,17 @@
 import { SettingsBase } from './settings-base';
 export interface IUserSettings {
     version: number;
+    theme: string;
     darkTheme: boolean;
     useZoomApp: boolean;
-    disableUserVideo: boolean;
-    disableUserAudio: boolean;
-    disableUserName: boolean;
 }
 
 export class UserSettings extends SettingsBase implements IUserSettings {
     // Default Values
     version = 3;
+    theme = 'blue';
     darkTheme = true;
     useZoomApp = false;
-    disableUserVideo = true;
-    disableUserAudio = true;
-    disableUserName = false;
 
     constructor(settings?: IUserSettings) {
         super();
