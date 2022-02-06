63553cee4ddc6f60c9fcacbfe8f5ae8aeb43d41d
Mikki Michaelis <mikkimichaelis@gmail.com>
2022-02-06 16:03:12 -0600

    attendance isValid

--- listings/attendance.ts
+++ listings/attendance.ts
@@ -140,7 +140,16 @@ export class Attendance extends Id implements IAttendance {
 
     // TODO validate based on length of attendance
     public isValid(): boolean {
-        return this.records.length > 2;   // require min three records to be valid
+        let valid = true;
+
+        valid = this.records.length > 2;   // require min three records to be valid
+        if (!valid) return valid;
+
+        valid = head(this.records).status === 'MEETING_ACTIVE_TRUE';
+        if (!valid) return valid;
+
+        valid = last(this.records).status === 'MEETING_ACTIVE_FALSE';
+        return valid;
     }
 
     // TODO ADD MASSIVE ERROR CHECKING!!!
