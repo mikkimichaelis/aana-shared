d7704155937535655afc8473be76b2ef653437bc
Mikki Michaelis <mikkimichaelis@gmail.com>
2022-02-27 13:33:08 -0600

    wip

--- models/userMember.class.ts
+++ models/userMember.class.ts
@@ -14,12 +14,12 @@ export class UserMember extends UserBase implements IUserMember {
     bday:string                 = '';
     activity: IUserActivity     = <any>null;
     
-    public get isOnline(): boolean {
-        const lastActivity: DateTime = DateTime.fromISO(this.activity.lastTime);
-        const diff = DateTime.utc().diff(lastActivity);
-        const online =  (diff.milliseconds / 1000 / 60) < 15;
-        return online;
-    }
+    // public get isOnline(): boolean {
+    //     const lastActivity: DateTime = DateTime.fromISO(this.activity.lastTime);
+    //     const diff = DateTime.utc().diff(lastActivity);
+    //     const online =  (diff.milliseconds / 1000 / 60) < 15;
+    //     return online;
+    // }
 
     public get daysSinceBday() {
         const bday:DateTime = DateTime.fromISO(this.bday);
