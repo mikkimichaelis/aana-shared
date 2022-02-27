215840189decc0ab03981fc56953c9b05fbd7ead
Mikki Michaelis <mikkimichaelis@gmail.com>
2022-02-27 13:19:46 -0600

    activity() wip

--- models/userActivity.class.ts
+++ models/userActivity.class.ts
@@ -4,24 +4,45 @@ import { DateTime } from 'luxon';
 import { IUserBase, UserBase } from './userBase.class';
 
 export interface IUserActivity extends IUserBase {
-    id: string,
-    name: string,
-    avatar: string,
-    lastLogon: string;
-    lastTime: string;
-    point: any; // FirePoint;
+    uid: string;
+
+    url: string;
+
+    lastAppRun: number;
+    lastAppRun$: string;
+
+    lastResume: number;
+    lastResume$: string;
+
+    timestamp: number;
+    timestamp$: string;
+
+    Aster_Michael: any;
+    Arlen_Remi: any;
 }
 
 export class UserActivity extends UserBase implements IUserActivity {
-    id: string          = '';
-    name: string        = '';
-    avatar: string      = '';
-    lastLogon: string   = '';
-    lastTime: string    = DateTime.utc().toISO();
-    point: any          = <any>null;
+    uid = '';
+    url = '';
 
-    constructor(activity?: any) {
+    lastAppRun = 0;
+    lastAppRun$ = '';
+
+    lastResume = 0;
+    lastResume$ = '';
+
+    timestamp = 0;
+    timestamp$ = '';
+
+    Aster_Michael = 'Go Away, Tickle Couch!';
+    Arlen_Remi = 'Princess Love (✿ ♥‿♥)'
+
+    constructor(data?: any) {
         super();
-        this.initialize(this, activity);
+        this.initialize(this, data);
+        this.uid = data.id;
+
+        this.timestamp = DateTime.now().toMillis();
+        this.timestamp$  = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
     }
 }
