65a9c2fe00c56bde6b3b4fd460a8e784e935e016
Mikki Michaelis <mikkimichaelis@gmail.com>
2023-03-16 01:44:54 -0500

    updated default user theme

--- models/user-settings.ts
+++ models/user-settings.ts
@@ -12,7 +12,7 @@ export interface IUserSettings {
 export class UserSettings extends SettingsBase implements IUserSettings {
     // Default Values
     version = 7;
-    theme = 'dark-myka-marie';
+    theme = 'dark-theme pink';
     darkTheme = true;
     useZoomApp = true;
     liveSearchLanguage = false;
