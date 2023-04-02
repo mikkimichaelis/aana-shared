de582f4e666f80a8517c0aa674162024dc699d4d
Mikki Michaelis <mikkimichaelis@gmail.com>
2023-04-02 17:37:09 -0500

    prayer-settings

---
+++ models/prayer-settings.ts
@@ -0,0 +1,27 @@
+import { SettingsBase } from './settings-base';
+
+export interface IPrayerSettings {
+    group: string,
+    random: boolean,
+    prayer: string,
+    reminders: boolean,
+    morning: number | null,
+    noon: number | null,
+    night: number | null
+}
+
+export class PrayerSettings extends SettingsBase implements IPrayerSettings {
+    group: string = 'NA';
+    random: boolean = true;
+    prayer: string = '';
+    reminders: boolean = false;
+    morning: number | null = null;
+    noon: number | null = null;
+    night: number | null = null;
+
+    version = 0;
+    constructor(settings?: IPrayerSettings) {
+        super();
+        this.update(settings, this.version);
+    }
+}
