24f23b9c0c95c6a62057d13371a8ecdc0bb4beb2
Mikki Michaelis <mikki@psychward.software>
2023-06-07 18:20:10 -0600

    wip

--- models/meeting-note.ts
+++ models/meeting-note.ts
@@ -1,17 +1,15 @@
 import { DateTime } from "luxon";
-import { Id, IId } from "./id.class";
+import { Base, IBase } from "./base.class";
 
 // This defines the persisted record for all meeting notes on a single day
 export interface IDailyNote {
-    id: string;     // record string index - millis start of day
+    date: string;     // record string index - millis start of day
     meetings: IMeetingNote[];
 }
 
-export interface IMeetingNote extends IId {
-    id: string;             // key in database - millis of start of day note is for
-    timestamp: number;     
-    date$: string;          // string of date note created
-
+export interface IMeetingNote extends IBase {
+    date: string;           // date of note (mirrors IDailyNote date)
+    
     uid: string;
     mid: string;
     name: string;           // meeting name
@@ -20,12 +18,9 @@ export interface IMeetingNote extends IId {
     html: string;
 }
 
-export class MeetingNote extends Id implements IMeetingNote {
+export class MeetingNote extends Base implements IMeetingNote {
 
-    public id: string = '';
-
-    public timestamp: number = 0;
-    public date$: string = ''
+    public date: string = DateTime.now().toFormat('yyyy-MM-dd')
 
     public uid: string = '';
     public mid: string = '';
@@ -35,13 +30,8 @@ export class MeetingNote extends Id implements IMeetingNote {
     public html: string = '<p></p>';
 
     constructor(note?: any) {
-        super(note);
+        super();
         this.initialize(this, note);
-
-        if (this.timestamp === 0) this.timestamp = DateTime.now().startOf('day').toMillis();
-        if (this.id === '') this.id = this.timestamp.toString();
-        
-        this.date$ = DateTime.fromMillis(this.timestamp).toISO();
     }
 
     public toObject() {
