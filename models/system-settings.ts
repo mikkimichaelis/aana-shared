c76c5b6b0612ff2c1a6d13fe11b9e00c0bef2881
Mikki Michaelis <mikki@psychward.software>
2023-07-24 20:23:57 -0500

    first use dashboard

--- models/system-settings.ts
+++ models/system-settings.ts
@@ -7,6 +7,7 @@ export interface ISystemSettings {
     app_version: string;
     first_run: boolean;
     first_use_journal: string;
+    first_use_dashboard: string;
 
     intro_tutorial: string;         // null: complete, otherwise string at which to start
     journal_tutorial: string;       // same...
@@ -26,13 +27,14 @@ export interface ISystemSettings {
 }
 
 export class SystemSettings extends SettingsBase implements ISystemSettings {
-    version = 5;                // version of these settings
+    version = 6;                // version of these settings
     app_version = <any>null;    // set to null to flag a new app install
                                 // app_version is then set in update.service
     first_run = true;
 
-    // ISO of first day journal free period
-    first_use_journal: string = DateTime.now().toISO();
+    // ISO of first day free period
+    first_use_journal: string = null;
+    first_use_dashboard: string = null;
 
     intro_tutorial = 'welcome';
     journal_tutorial = 'start';
