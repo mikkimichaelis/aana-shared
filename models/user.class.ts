4c4467c8ec97a4b1b573c835b41dcbe8dff3b021
Mikki Michaelis <mikki@psychward.software>
2023-10-13 10:58:41 -0500

    add fellowships

--- models/user.class.ts
+++ models/user.class.ts
@@ -145,6 +145,7 @@ export interface IUserPreferences {
     location_value: string;
     ninety_start: number;   // date to start 90/90
     meetingMinutesChartDuration: any;
+    fellowships: string[];
 }
 export class UserProfile extends Base implements IUserProfile {
     anonymous: boolean = true;
@@ -233,7 +234,8 @@ export class User extends UserBase implements IUser {
         location: false,
         location_value: '',
         ninety_start: DateTime.now().startOf('day').toMillis(),
-        meetingMinutesChartDuration: '7'
+        meetingMinutesChartDuration: '7',
+        fellowships: []
     }
     profile: IUserProfile = <any>null;
     homeMeeting: string = <any>null;
