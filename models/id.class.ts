51df5f790d1dd1de6e614e3bbde6c238b7321595
Mikki Michaelis <mikkimichaelis@gmail.com>
2024-07-27 23:23:08 -0500

    remove uuid causing build problems

--- models/id.class.ts
+++ models/id.class.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import { Base, IBase } from './base.class';
 export interface IId extends IBase {
     id: string;
@@ -9,7 +9,7 @@ export interface IId extends IBase {
 }
 
 export class Id extends Base implements IId {
-    id: string  = uuidv4();
+    id: string  = ''; // uuidv4();
     //createdAt = firebase.firestore.FieldValue.serverTimestamp();
     //updatedAt = firebase.firestore.FieldValue.serverTimestamp();
 
