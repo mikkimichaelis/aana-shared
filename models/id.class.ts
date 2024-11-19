c417b682ec048a7b025efab04d689f88e7333359
mikkimonroe <mikki@recoverysky.org>
2024-11-18 20:33:06 -0600

    reverted to uuid for id

--- models/id.class.ts
+++ models/id.class.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Base, IBase } from './base.class';
-import { ObjectId } from 'bson'
+// import { ObjectId } from 'bson'
 export interface IId extends IBase {
     id: string;
 
@@ -10,7 +10,8 @@ export interface IId extends IBase {
 }
 
 export class Id extends Base implements IId {
-    id: string  = new ObjectId().toHexString();
+    id: string = uuidv4();
+    // id: string  = new ObjectId().toHexString();
     //createdAt = firebase.firestore.FieldValue.serverTimestamp();
     //updatedAt = firebase.firestore.FieldValue.serverTimestamp();
 
